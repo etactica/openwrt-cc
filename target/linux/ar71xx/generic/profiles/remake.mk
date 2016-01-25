7ba986d64da2be61279b6f9c9e0822fb30f7752f
Karl Palsson <karlp@remake.is>
2016-01-25 11:10:54 +0000

    rme: eg200: ensure oneshot led trigger module is included.
    We use this for the modbus activity light.

--- target/linux/ar71xx/generic/profiles/remake.mk
+++ target/linux/ar71xx/generic/profiles/remake.mk
@@ -7,7 +7,8 @@
 
 define Profile/RME_EG200
 	NAME:=ReMake Electric EG200
-	PACKAGES:=kmod-usb-core kmod-usb2 kmod-ledtrig-usbdev \
+	PACKAGES:=kmod-usb-core kmod-usb2 \
+		kmod-ledtrig-oneshot kmod-ledtrig-usbdev \
 		kmod-fs-vfat kmod-fs-msdos \
 		kmod-usb-serial kmod-usb-serial-ftdi \
 		kmod-usb-storage kmod-usb-storage-extras \
